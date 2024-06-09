8961c9341c57e887e5f10bec9c648bd04144c23b
Naveen M K <naveen521kk@gmail.com>
2024-06-09 19:32:27 +0530

    some fixups for mobile nav

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -22,6 +22,24 @@ export function Header() {
           </Link>
           <ModeToggle />
         </nav>
+        <nav className="md:hidden flex flex-row justify-between items-center w-full">
+          <div className="flex flex-row gap-2">
+            <Link
+              href="#"
+              className="flex items-center gap-2 text-lg font-semibold md:text-base"
+            >
+              <Package2 className="h-6 w-6" />
+              <span className="sr-only">Task Management App</span>
+            </Link>
+            <Link
+              href="#"
+              className="text-foreground transition-colors hover:text-foreground"
+            >
+              Task Management App
+            </Link>
+          </div>
+          <ModeToggle />
+        </nav>
       </header>
     </>
   );
