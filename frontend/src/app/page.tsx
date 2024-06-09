4a9039cec723d0d7f891134dca4ed0276d774124
Naveen M K <naveen521kk@gmail.com>
2024-06-09 14:10:05 +0530

    Add dark mode support

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,17 @@
+import { readOwnItemsUsersMeItemsGet } from "@/client";
+import { Header } from "@/components/header";
+import { cookies } from "next/headers";
+
 export default function Home() {
+  console.log(cookies())
+  // const items = readOwnItemsUsersMeItemsGet();
+  // console.log(items);
   return (
-    <main>
-      
-    </main>
+    <div className="flex min-h-screen w-full flex-col">
+      <Header />
+      <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
+        
+      </main>
+    </div>
   );
 }
